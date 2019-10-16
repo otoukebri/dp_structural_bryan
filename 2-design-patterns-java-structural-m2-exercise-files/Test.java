cdc87c3f31dcd40aaae00c7f8982050641b3921b
samy <39716963+samykhelifa@users.noreply.github.com>
2019-10-16 14:04:26 +0200

    Update Test.java

--- 2-design-patterns-java-structural-m2-exercise-files/Test.java
+++ 2-design-patterns-java-structural-m2-exercise-files/Test.java
@@ -1,1 +1,13 @@
+import java.util.List;
+
+public class AdapterDemo {
+
+	public static void main(String[] args) {
+		EmployeeClient client = new EmployeeClient();
+		
+		List<Employee> employees = client.getEmployeeList();
+		
+		System.out.println(employees);
+	}
+}
 
