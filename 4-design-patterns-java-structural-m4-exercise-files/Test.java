7a255ddde752689f7db1f0b306d98fbf8108c962
samy <39716963+samykhelifa@users.noreply.github.com>
2019-10-21 22:05:14 +0200

    Update Test.java

--- 4-design-patterns-java-structural-m4-exercise-files/Test.java
+++ 4-design-patterns-java-structural-m4-exercise-files/Test.java
@@ -1,7 +1,9 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Iterator;
 
 public class Test{
-
-	public static void main(String[] args) {
+      public static void main(String[] args) {
 		Menu mainMenu = new Menu("Main", "/main");
 		MenuItem safetyMenuItem = new MenuItem("Safety", "/safety");
 		mainMenu.add(safetyMenuItem);
