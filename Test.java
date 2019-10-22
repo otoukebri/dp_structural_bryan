c4fc1f8f80726141678d7616b45e5b677de5b84f
samy <39716963+samykhelifa@users.noreply.github.com>
2019-10-22 02:14:26 +0200

    Update Test.java

--- Test.java
+++ Test.java
@@ -3,27 +3,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 class Test {
-	
-	String name;
-	String url;
+    public static void main(String[] args) {
 	List<String> li = new ArrayList<>();
 	li.add("sam");li.add("ryio");li.add("caro");
-	
-	public String getName() {
-		return name;
-	}
-	
-	public String getUrl() {
-		return url;
-	}	
-	
-//	public abstract String toString();
-	
-	String print(MenuComponent menuComponent) {
-		StringBuilder builder = new StringBuilder(name);
-		builder.append(": ");
-		builder.append(url);
-		builder.append("\n");
-		return builder.toString();
+	StringBuilder builder = new StringBuilder();
+	    
+	Iterator<String> itr = li.iterator();
+		while(itr.hasNext()) {
+			String s = itr.next();
+			builder.append(s);
+		}
+	System.out.println(builder);
+		
 	}
 }
