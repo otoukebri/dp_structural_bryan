ba1219d8798ff596f121ed8d563c3a31039d0ecf
samy <39716963+samykhelifa@users.noreply.github.com>
2019-10-23 06:55:00 +0200

    Update Test.java

--- 5-design-patterns-java-structural-m5-exercise-files/decorator/Test.java
+++ 5-design-patterns-java-structural-m5-exercise-files/decorator/Test.java
@@ -4,7 +4,7 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.OutputStream;
 
-public class DecoratorEverydayDemo {
+public class Test {
 
 	public static void main(String args []) throws Exception {
 		File file = new File("./output.txt");
