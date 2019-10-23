71dcecfeb35fd28402a855c78e380273524e0ea9
samy <39716963+samykhelifa@users.noreply.github.com>
2019-10-23 08:06:47 +0200

    Update Test.java

--- 5-design-patterns-java-structural-m5-exercise-files/decorator/Test.java
+++ 5-design-patterns-java-structural-m5-exercise-files/decorator/Test.java
@@ -9,5 +9,11 @@ public class Test {
 	public static void main(String args []) throws Exception {
 		File file = new File("./output.txt");
 		file.createNewFile();
+		OutputStream oStream = new FileOutputStream(file);
+		DataOutputStream doStream = new DataOutputStream(oStream);
+		doStream.writeChars("text");
+		
+		doStream.close();
+		oStream.close();
   }
 		
